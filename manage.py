031dfe573dee7577c2194f887c1ac596f40613f7
Pandada8 <pandada8@gmail.com>
2015-07-13 22:49:36 +0800

    Fix manager Error

--- manage.py
+++ manage.py
@@ -11,14 +11,18 @@ def init():
     if not app.config['DEBUG']:
         print('请使用 PostgreSQL 的 createdb 创建数据库')
         input("创建完成后回车")
-    model.init_db()
+        print("正在初始化数据库表")
+        model.init_db()
+    else:
+        print("正在初始化数据库表")
+        model.init_db()
 
-    print('初始化Secret Key')
-    with open('nautiloidea/config.py') as fp:
-        content = fp.read()
-        content = re.sub(r'^secret_key \= [\s\S]+?$', "secret_key = {}".format(repr(os.urandom(40))), content, flags=re.MULTILINE)
-    with open('nautiloidea/config.py', 'w') as fp:
-        fp.write(content)
+    # print('初始化Secret Key')
+    # with open('nautiloidea/config.py') as fp:
+    #     content = fp.read()
+    #     content = re.sub(r'^secret_key \= [\s\S]+?$', "secret_key = {}".format(repr(os.urandom(40))), content, flags=re.MULTILINE)
+    # with open('nautiloidea/config.py', 'w') as fp:
+    #     fp.write(content)
 
     print('创建上传目录')
     os.makedirs(app.config['UPLOAD'], exist_ok=True)

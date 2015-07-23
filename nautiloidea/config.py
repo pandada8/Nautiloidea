4021b1aae177076b56e13e1f6295b9dcda5d959b
Pandada8 <pandada8@gmail.com>
2015-07-23 14:22:24 +0800

    add config sample

--- nautiloidea/config.py
+++ nautiloidea/config.py
@@ -1,8 +1,8 @@
 import os
 
-secret_key = b'`\xcf\x89}HC\x97\x1b\xe10\x06@\xee\xf3#\x9c\xaf\x90g\xe2X\x11;QrI\xc0\x0b\xc0\x8d\xcfOX+\xa4\xadj\xe9\x9c\xf9'
-app_id = 2100130300
-app_secret = "bcd24dab3c8711033901b672635ef465"
+secret_key = '<RandomText>'
+app_id = 12345 # <your app id>
+app_secret = "secret" #<Your app secret>
 
 class DevSetting:
     UPLOAD = os.path.normpath(os.path.join(os.path.split(__file__)[0], '../upload'))
---
+++ nautiloidea/static/src/config.jsx
@@ -0,0 +1,1 @@
+export var ak = "<You Key>"
